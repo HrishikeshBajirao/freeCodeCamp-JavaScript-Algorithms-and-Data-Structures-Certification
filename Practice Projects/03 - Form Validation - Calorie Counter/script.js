d3de76dbb54d21024ce6f644f0a1305e50328d7e
HrishikeshBajirao <65894264+HrishikeshBajirao@users.noreply.github.com>
2024-06-25 15:24:30 +0530

    Update and rename script,js to script.js

--- Practice Projects/03 - Form Validation - Calorie Counter/script.js
+++ Practice Projects/03 - Form Validation - Calorie Counter/script.js
@@ -1,6 +1,6 @@
  # 03 - Form Validation - Calorie Counter
 
- const calorieCounter = document.getElementById('calorie-counter');
+const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
